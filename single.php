4b6f52e0ec1094707b355d8f784c0da0511bf931
Toru Miki <waviaei@gmail.com>
2014-08-24 19:53:48 +0900

    Unlined icons. Changed link colours

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 large-8 large-offset-2 columns" role="main">
 
 	<?php do_action('foundationPress_before_content'); ?>
 
@@ -35,7 +35,8 @@
 
 	<?php do_action('foundationPress_after_content'); ?>
 
-	</div>
 	<?php get_sidebar(); ?>
+	</div>
+
 </div>
 <?php get_footer(); ?>
