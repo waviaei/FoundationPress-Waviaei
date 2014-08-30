31f2626399778326a0db2fa04b6cdd8c15a20355
Toru Miki <waviaei@gmail.com>
2014-08-30 15:58:47 +0900

    Changed archive.php to use content-archive.php, which uses excerpt instead the content

--- archive.php
+++ archive.php
@@ -7,7 +7,7 @@
 
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php get_template_part( 'content', 'archive' ); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
---
+++ content-archive.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The default template for displaying content. Used for both single and index/archive/search.
+ *
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php FoundationPress_entry_meta(); ?>
+	</header>
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div>
+	<hr />
+</article>
